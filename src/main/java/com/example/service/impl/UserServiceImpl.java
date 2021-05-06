106a75fa27b641109c274c33617174ce4faee819
k.rajkumar5810@gmail.com <k.rajkumar5810@gmail.com>
2021-05-06 22:29:43 +0530

    add service classes

---
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.service.IUserService;
+
+@Service
+public class UserServiceImpl implements IUserService{
+
+}
